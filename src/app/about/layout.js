2b28c51992efb3fbe9eb0e44e2c9d696fed36c27
pushpendrapatel8055 <pushpendrapatel8055@gmail.com>
2023-09-15 10:13:03 +0530

    conditional routing

--- src/app/about/layout.js
+++ src/app/about/layout.js
@@ -1,13 +1,21 @@
-"useclient"
+"use client"
 import Link from "next/link"
 
 import "./about.css"
 import Footer from "./Footer"
+import { usePathname } from "next/navigation"
 
 // this is common layout for about us
+
+
 const Layout = ({children}) => {
+  let pathname = usePathname()
   return (
     <div>
+
+    {
+      pathname !== "/about/aboutStudent" ?
+
      <ul className="links">
         <li>
             <h2 style={{margin:0}} >School</h2>
@@ -26,7 +34,7 @@ const Layout = ({children}) => {
             <Link href="/about/aboutStudent" >About Student</Link>
 
         </li>
-     </ul>
+     </ul> : <Link href="/about" >go to about page</Link> }
      {children}
 
 
